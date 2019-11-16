85a13d307168c4d542fc07ca28c3f89fbce64cec
Sebastian Wels-Lopez <seby_wels@hotmail.com>
2019-11-16 02:37:29 -0800

    Place tile on top of clicked tile

--- Gem Tower Defense/Assets/Sciprts/MouseDownScript.cs
+++ Gem Tower Defense/Assets/Sciprts/MouseDownScript.cs
@@ -5,7 +5,7 @@ using UnityEngine.EventSystems;
 
 public class MouseDownScript : MonoBehaviour, IPointerClickHandler
 {
-    public BoardManager boardManager;
+    private BoardManager boardManager;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,6 +26,8 @@ public class MouseDownScript : MonoBehaviour, IPointerClickHandler
     public void OnPointerClick(PointerEventData eventData)
     {
         Debug.Log("I pointerClickHandled the thing");
-        boardManager.PlaceTile(4, 5);
+        var x = gameObject.transform.position.x;
+        var y = gameObject.transform.position.y;
+        boardManager.PlaceTile(x, y);
     }
 }
