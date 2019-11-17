4215131b11ff08177c892f9b7fef30031c528805
Sebastian Wels-Lopez <seby_wels@hotmail.com>
2019-11-16 17:42:20 -0800

    Units travel along hard-coded waypoints and die at the last checkpoint

--- Gem Tower Defense/Assets/Sciprts/Enemy.cs
+++ Gem Tower Defense/Assets/Sciprts/Enemy.cs
@@ -7,18 +7,28 @@ public class Enemy : MonoBehaviour
 {
     public float hp;
     public float speed;
-    private Vector3 target;
+    private Vector3 currentTarget;
+    private int targetNumber = 0;
+    private Vector3[] targets = new Vector3[3];
 
     // Start is called before the first frame update
     void Start()
     {
-        speed = 1f;
+        speed = 3f;
+        LoadTargets();
         GetInitialTarget();
+    }
+
+    private void LoadTargets()
+    {
+        targets[0] = new Vector3(8, 3, -1);
+        targets[1] = new Vector3(8, 6, -1);
+        targets[2] = new Vector3(19, 6, -1);
     }
 
     private void GetInitialTarget()
     {
-        target = new Vector3(8, 3, -1);
+        currentTarget = targets[0];
     }
 
     // Update is called once per frame
@@ -26,8 +36,15 @@ public class Enemy : MonoBehaviour
     {
         // move
         float step = speed * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(transform.position, target, step);
+        transform.position = Vector3.MoveTowards(transform.position, currentTarget, step);
 
         // check if target reached
+        if (transform.position == currentTarget)
+        {
+            if (targetNumber == targets.Length - 1) Destroy(gameObject);
+
+            targetNumber++;
+            currentTarget = targets[targetNumber];
+        }
     }
 }
