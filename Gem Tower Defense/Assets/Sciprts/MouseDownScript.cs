be155543f0fe4928bb59d76ff01833331d36084d
Sebastian Wels-Lopez <seby_wels@hotmail.com>
2019-11-19 03:44:05 -0800

    Sprites darken when selected and get restored when deselected

--- Gem Tower Defense/Assets/Sciprts/MouseDownScript.cs
+++ Gem Tower Defense/Assets/Sciprts/MouseDownScript.cs
@@ -7,12 +7,14 @@ public class MouseDownScript : MonoBehaviour, IPointerClickHandler, ISelectHandl
 {
     private BoardManager boardManager;
     private EventSystem eventSystem;
+    private SpriteRenderer spriteRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
         boardManager = (BoardManager)GameObject.Find("GameBoard").GetComponent(typeof(BoardManager));
         eventSystem = EventSystem.current;
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -31,18 +33,34 @@ public class MouseDownScript : MonoBehaviour, IPointerClickHandler, ISelectHandl
         Debug.Log("I pointerClickHandled the thing");
         var x = gameObject.transform.position.x;
         var y = gameObject.transform.position.y;
-        boardManager.PlaceRandomTile(x, y);
+        //boardManager.PlaceRandomTile(x, y);
 
         eventSystem.SetSelectedGameObject(gameObject);
+    }
+
+    private void DarkenSpriteColor()
+    {
+        Color newColor = spriteRenderer.color;
+        newColor.a = 0.8f;
+        spriteRenderer.color = newColor;
+    }
+
+    private void RestoreSpriteColor()
+    {
+        Color newColor = spriteRenderer.color;
+        newColor.a = 1f;
+        spriteRenderer.color = newColor;
     }
 
     public void OnDeselect(BaseEventData eventData)
     {
         Debug.Log("I got selected");
+        RestoreSpriteColor();
     }
 
     public void OnSelect(BaseEventData eventData)
     {
         Debug.Log("I got deselected");
+        DarkenSpriteColor();
     }
 }
