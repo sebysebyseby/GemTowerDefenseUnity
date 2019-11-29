891584a7b5f883987f90cae0bd0fba41fc1ef76f
Sebastian Wels-Lopez <seby_wels@hotmail.com>
2019-11-29 00:27:51 -0800

    When a mundane gem gets selected a range indicator appears around it

---
+++ Gem Tower Defense/Assets/Sciprts/CircleDrawer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public static class CircleDrawer
+{
+
+    public static void DrawCircle(this GameObject container, float radius, float lineWidth)
+    {
+        Material mat = Resources.Load("SelectedCircle", typeof(Material)) as Material;
+
+        var segments = 360;
+        var line = container.AddComponent<LineRenderer>();
+        line.useWorldSpace = false;
+        line.startWidth = lineWidth;
+        line.endWidth = lineWidth;
+        line.startColor = Color.blue;
+        line.endColor = Color.cyan;
+        line.material = mat;
+        line.positionCount = segments + 1;
+
+        var pointCount = segments + 1; // add extra point to make startpoint and endpoint the same to close the circle
+        var points = new Vector3[pointCount];
+
+        for (int i = 0; i < pointCount; i++)
+        {
+            var rad = Mathf.Deg2Rad * (i * 360f / segments);
+            points[i] = new Vector3(Mathf.Sin(rad) * radius,  Mathf.Cos(rad) * radius, -2);
+        }
+
+        line.SetPositions(points);
+    }
+}
--- Gem Tower Defense/Assets/Sciprts/MundaneGem.cs
+++ Gem Tower Defense/Assets/Sciprts/MundaneGem.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class MundaneGem : GameboardEntity
 {
     public string type;
+    public float range = 2;
+
     // Start is called before the first frame update
     public override void Start()
     {
@@ -15,6 +18,18 @@ public class MundaneGem : GameboardEntity
     public override void UpdateDescription()
     {
         description.text = "Type of mundane gem selected: " + type;
+    }
+
+    public override void OnSelect(BaseEventData eventData)
+    {
+        base.OnSelect(eventData);
+        gameObject.DrawCircle(range, 0.1f);
+    }
+
+    public override void OnDeselect(BaseEventData eventData)
+    {
+        base.OnDeselect(eventData);
+        Destroy(gameObject.GetComponent<LineRenderer>());
     }
 
     // Update is called once per frame
