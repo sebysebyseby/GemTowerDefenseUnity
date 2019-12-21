2e7f2c1c3675aa03b59b54b316d973f42c64b5b1
Sebastian Wels-Lopez <seby_wels@hotmail.com>
2019-12-21 15:20:50 -0800

    Display current level

--- Gem Tower Defense/Assets/Sciprts/TextElements/LevelText.cs
+++ Gem Tower Defense/Assets/Sciprts/TextElements/LevelText.cs
@@ -1,18 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelText : MonoBehaviour
 {
+    BoardManager boardManager;
+    Text text;
     // Start is called before the first frame update
     void Start()
     {
-        
+        text = gameObject.GetComponent<Text>();
+        boardManager = (BoardManager)GameObject.Find("GameBoard").GetComponent(typeof(BoardManager));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        text.text = "Level: " + boardManager.level.ToString();
     }
 }
